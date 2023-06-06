c9b26baae03afcf3eee8b13b6d39cccbe6307b95
bryangomezcarranza <bryangomezcarranza@gmail.com>
2023-06-06 13:54:03 -0700

    Finishes day of weeks

--- js/daily-grind.js
+++ js/daily-grind.js
@@ -1,59 +1,131 @@
-/* 
-pic - image src 
-alt - alt tag for the page 
-color - color to match image 
-description - description of coffee
-name - name of coffee
-day  - day of the week 
+/*
+
+    Here are the items we believe we need to produce for each day's coffee
+
+    pic - image scr
+    alt - the alt tag for the image
+    color - color to match image
+    desc - description of the coffee
+    name - name of the coffee
+    day - day of the week
+
 
 */
-let myDate = new Date();
-let today = myDate.getDay();
 
-function coffeTemplate(coffee) {
-    return `
-    <p>
-    <img src="${coffee.image}" alt="${coffee.alt}" id="coffee" />
+function coffeeTemplate() {
+    return `<p>
+    <img src="${coffee.pic}" alt="${coffee.alt}" id="coffee" />
     <strong class="feature">${coffee.day} Coffee Special:</strong> ${coffee.day} daily coffee special is <strong class="feature">${coffee.name}</strong>, ${coffee.desc}</p>
-    <p>
-
-    `; 
+    `;
 }
 
-switch(today) {
+let myDate = new Date();
+
+let coffee = "";
+
+let today = myDate.getDay();
+
+//use location object to access querystring (address bar)
+const queryString = window.location.search;
+    
+//output to console    
+console.log(queryString);
+    
+//separate query string parameters
+const urlParams = new URLSearchParams(queryString);
+
+//today = 3;
+
+if(urlParams.has("day")){//from querystring
+    today = urlParams.get("day");
+ }
+
+ today = parseInt(today);
+
+switch (today) {
+
+    case 0:
+        today = "Sunday";
+        break;
+
     case 1:
-        today = "Monday;";
-        coffee = { 
-            name: "Bubble Tea", 
-            color: "Pink ",
-            image: "images/bubble-tea.jpg", 
+        today = "Monday";
+        coffee = {
+            name: "Bubble Tea",
+            color: "pink",
+            pic: "images/bubble-tea.jpg",
             alt: "A picture of a bubble tea",
-            day: "Wednesday",
-            desc: `I like me some bubble tea`
+            day: "Monday",
+            desc: `I like me some bubble tea!`
         }
-        
-    case 2: 
-       today = "Tuesday";        
-       break;
-    case 3: 
-       today = "Wednesday";        
+        break;
+
+    case 2:
+        today = "Tuesday";
+        coffee = {
+            name: "Drip Coffee",
+            color: "black",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        }
+        break;
+
+    case 3:
+        today = "Wednesday";
+        coffee = {
+            name: "Caramel Latte",
+            color: "yellow",
+            pic: "images/caramel.latte.jpg",
+            alt: "A picture of a Caramel Latte",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        }
+        break;
+
+    case 4:
+        today = "Thursday";
+        coffee = {
+            name: "Drip Coffee",
+            color: "black",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        }
+        break;
+
+    case 5:
+        today = "Friday";
+        coffee = {
+            name: "Drip Coffee",
+            color: "black",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        }
+        break;
+
+    case 6:
+        today = "Saturday";
+        coffee = {
+            name: "Drip Coffee",
+            color: "black",
+            pic: "images/drip.jpg",
+            alt: "A picture of a drip coffee",
+            day: "Tuesday",
+            desc: `Give it to me straight!`
+        }
         break;
 
     default:
-        today = "Not sure what day it is. Error: There is an error";
+        today = "Not sure what day it is!";
 }
 
-document.getElementById("coffee-cup").innerHTML = coffeTemplate(coffee)
+document.getElementById("coffee-cup").innerHTML = coffeeTemplate(coffee);
+document.querySelector("html").style.backgroundColor = coffee.color;
 
-// How to set an object 
-coffee = { 
-    name: "Bubble Tea", 
-    color: "Pink ",
-    image: "images/bubble-tea.jpg", 
-    alt: "A picture of a bubble tea",
-    day: "Wednesday",
-    desc: `I like me some bubble tea`
-}
-
-//alert(today)
+//alert(today);
 console.log(coffee);
